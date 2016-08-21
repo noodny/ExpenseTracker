88c5709292b6bb27d9f12de9ed94575ab1fe8c5a
Krzysztof Bury <krzysiek.bury@gmail.com>
2016-08-21 12:44:51 +0200

    Changes to SQL scripts.

--- DB/BANK_ACCOUNT_TYPES.sql
+++ DB/BANK_ACCOUNT_TYPES.sql
@@ -3,8 +3,10 @@ DROP TABLE IF EXISTS BANK_ACCOUNT_TYPES CASCADE;
 CREATE TABLE BANK_ACCOUNT_TYPES (
 	ID_BA_TYP serial PRIMARY KEY,
 	BA_TYPE varchar (50) NOT NULL,
-	BA_DESC varchar (250) NULL,
+	BA_DESC varchar (250) NULL,	
 	ACTIVE smallint NOT NULL default 1,
+	IS_COMMON_ACCOUNT smallint NOT NULL default 0,
+	ID_BA_OWN integer REFERENCES BANK_ACCOUNT_OWNER (ID_BA_OWN),
 	INSERT_DATE timestamp default current_timestamp,
 	UPDATE_DATE timestamp default current_timestamp
 );
