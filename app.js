acfe3bbe870acf0ee6eca3e251dd57a37d4265ed
Krzysiek Jakubik <krzysiek.jakubik@gmail.com>
2017-08-24 21:22:20 +0100

    Use environment variable for application port setting.
    It's a good practice and might be useful.

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ const db = require('./queries');
 
 app.get('/api/transaction_categories', db.getTransactionCategories);
 
-app.listen(8080, function() {
+app.listen(process.env.PORT || 8080, function() {
     console.log('Server is listening ....');
 });
